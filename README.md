627a02c935ff10533e45d530be00af908c226748
Varun <varun.pal@myntra.com>
2016-06-16 17:23:45 +0530

    Version 1.0.0.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # diffback
 Fire callbacks whenever properties of an object change.
+
+No observers etc. As performant as changing the value and manually executing the callbacks.
+
+### Example
+
+```javascript
+var diffback = require('diffback');
+
+function print(newValue, oldValue) {
+  console.log(newValue, oldValue);
+}
+var object = {
+  a: 10,
+  b: "hello"
+};
+
+diffback(object, 'z', print);
+
+object.z = 100; // Prints: 100 (new value) undefined (old value)
+
+object.z = 200; // Prints: 200 (new value) 100 (old value)
+
+diffback.remove(object, 'z', print); // removes print callback
+
+```
--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var attach = function (object, property, callback) {
     object.mappings[property] = [callback];
     Object.defineProperty(object, property, {
       set: function (value) {
-        var oldValue = this.value;
+        var oldValue = this.value || object[property];
         this.value = value;
         if (object.mappings[property] && object.mappings[property].length) {
           object.mappings[property].forEach(function (callback) {
@@ -36,5 +36,27 @@ var attach = function (object, property, callback) {
   }
 };
 
+var remove = function (object, property, callback) {
+  if (!object) {
+    console.error('Invalid object');
+    return;
+  }
+  if (!property || typeof property === 'function') {
+    console.error('Invalid object property');
+    return;
+  }
+  if (typeof callback !== 'function') {
+    console.error('Callback should be a function');
+    return;
+  }
+
+  if (object.mappings && object.mappings[property]) {
+    object.mappings[property] = object.mappings[property].filter(function (item) {
+      return item !== callback;
+    });
+  }
+};
+
 module.exports = attach;
+module.exports.remove = remove;
 
